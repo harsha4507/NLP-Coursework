6e327b94f3dc69503a38a18f43592966d7afefa0
SaiMounika138 <84349945+SaiMounika138@users.noreply.github.com>
2021-05-25 20:51:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-#from sklearn.externals import joblib
-#import gensim
-#import gensim.corpora as corpora
-#import gensim.models as models
-#from gensim.test.utils import common_corpus, common_dictionary
-#from gensim.models import CoherenceModel
-#import pyLDAvis.gensim_models
-import pickle
-#import pyLDAvis
-#import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -22,18 +10,38 @@ def home():
 @app.route('/predict',methods=['GET','POST'])
 def predict():
 	model1 = open('model.pkl','rb')
+	df1= open('dataframe.pkl','rb')
 	c1=request.form.get('c1')
 	c2=request.form.get('c2')
 	c3=request.form.get('c3')
-	x = joblib.load(model1)
+	c4=request.form.get('c4')
+	c5=request.form.get('c5')
+	x = pickle.load("model1.pkl","rb")
+	P=pd.DataFrame()
+	k=pd.read_pickle(df1)
+	k10=k.to_html()
 	a=0
 	if request.method == 'POST':
 		if c1:
-			a="1"
+			k1=k.loc[k['Company']=='O2']
+			P=P.append(k1,ignore_index=True)
+		if c2:
+			k2=k.loc[k['Company']=='Safaricorn_Care']
+			P=P.append(k2,ignore_index=True)
+		if c3:
+			k3=k.loc[k['Company']=='VerizonSupport']
+			P=P.append(k3,ignore_index=True)
+		if c4:
+			k4=k.loc[k['Company']=='idea_cares']
+			P=P.append(k4,ignore_index=True)
+		if c5:
+			k5=k.loc[k['Company']=='sprintcare']
+			P=P.append(k5,ignore_index=True)
+		P=P.to_html()
 		y= x.print_topics()
 		z=pd.DataFrame(y)
 		z=z.to_html()
-	return render_template('result.html',predict=z, pre=a)
+	return render_template('result.html',predict=z, pre=a,df=P)
 
 if __name__ == '__main__':
 	app.run(debug=True)
