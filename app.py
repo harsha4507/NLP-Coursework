2cecf682ab90f586d7acfa7600c8391cbfff6e9c
harsha4507 <56903066+harsha4507@users.noreply.github.com>
2021-05-26 13:56:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,30 +34,30 @@ def predict():
 			k1=k.loc[k['Company']=='O2']
 			P=P.append(k1,ignore_index=True)
 			a=1
-			Hist_session(c1)
+			Hist_session.append(c1)
 		if c2:
 			k2=k.loc[k['Company']=='Safaricom_Care']
 			P=P.append(k2,ignore_index=True)
 			a=1
-			Hist_session(c2)
+			Hist_session.append(c2)
 		if c3:
 			k3=k.loc[k['Company']=='VerizonSupport']
 			P=P.append(k3,ignore_index=True)
 			a=1
-			Hist_session(c3)
+			Hist_session.append(c3)
 		if c4:
 			k4=k.loc[k['Company']=='idea_cares']
 			P=P.append(k4,ignore_index=True)
 			a=1
-			Hist_session(c4)
+			Hist_session.append(c4)
 		if c5:
 			k5=k.loc[k['Company']=='sprintcare']
 			P=P.append(k5,ignore_index=True)
 			a=1
-			Hist_session(c5)
+			Hist_session.append(c5)
 		if a==0:
 			P=P.append(k,ignore_index=True)
-			Hist_session([c1,c2,c3,c4,c5])
+			Hist_session.append([c1,c2,c3,c4,c5])
 		P=P.to_html()
 		y= x.print_topics()
 		z=pd.DataFrame(y)
